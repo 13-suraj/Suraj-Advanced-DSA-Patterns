4d0e559001c07a4e7a7329a5a5e8e732dcc30bc3
Suraj Keshari <surajkeshari260@gmail.com>
2025-09-27 13:18:25 +0530

    Added Median of 2 Sorted Array Alogorithm

--- BinarySearchVariation.java
+++ BinarySearchVariation.java
@@ -104,6 +104,35 @@ public class BinarySearchVariation {
         return -1; // It means Array is not Rotated, it is normally sorted.
     }
 
+    public static double findMedianSortedArrays(int[] a, int[] b) {   // Hard Problem
+        int n1 = a.length, n2 = b.length;
+        if(n1 > n2) return findMedianSortedArrays(b, a);
+
+        int l = 0, r = n1;
+        int total = n1 + n2;
+        int left = (n1 + n2 + 1) / 2;
+
+        while(l <= r) {
+            int mid1 = l + (r - l) / 2;
+            int mid2 = left - mid1;
+
+            int l1 = Integer.MIN_VALUE, l2 = Integer.MIN_VALUE;
+            int r1 = Integer.MAX_VALUE, r2 = Integer.MAX_VALUE;
+
+            if(mid1 < n1) r1 = a[mid1];
+            if(mid2 < n2) r2 = b[mid2];
+            if(mid1 > 0) l1 = a[mid1 - 1];
+            if(mid2 > 0) l2 = b[mid2 - 1];
+            if(l1 <= r2 && l2 <= r1){
+                if(total % 2 == 1) return Math.max(l1, l2);
+                return ((double)(Math.max(l1,l2) + Math.min(r1,r2))) / 2.0;
+            }
+            else if(l1 > r2) r = mid1 - 1;
+            else l = mid1 + 1;
+        }
+        return 0; 
+    }
+
     public static void main(String[] args) {
         int[] numbers = {22,20,17,14,11,7,4,1};                          // Normal Sorted in Decreasing-Order
         System.out.println(binarySearchOrderAgnostic(numbers, 17,0,7));  // answer = 2
@@ -136,3 +165,4 @@ In the beginning of the loop,
     if 'start' pointer was pointing to the possible answer :--- 'end' pointer will point to the answer after the loop is over. 
 
 */
+
