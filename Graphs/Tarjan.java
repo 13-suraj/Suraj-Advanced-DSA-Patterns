f1b4e2d958b0cbdea46ea96555f66fb5742ac755
Suraj Keshari <surajkeshari260@gmail.com>
2026-01-17 17:45:16 +0530

    Updated Tarjan's algorithm
    Added functionality to detect articulation points in the graph.

--- Graphs/Tarjan.java
+++ Graphs/Tarjan.java
@@ -3,12 +3,20 @@ import java.util.*;
 
 public class Tarjan {
 
-    // Tarjan's Algorithm :-  This algorithm is used for finding bridges(critical edges) in a graph.
+    // Tarjan's Algorithm :-  This algorithm is used for finding bridges(critical edges), Articulation Point(Node/Vertex) in a graph.
 
     // What is bridge or critical edge?
     // -> Bridge or critical edge in a graph is the edge which can split the graph into two different components
     //    if we remove it from the edge.
 
+    // What is an Articulation Point?
+    // -> Articulation Point in a graph is that node/vertex which can split the graph into two different components
+    //    if we remove it from the edge.
+
+    // There can be multiple bridges and articulation points in a graph. 
+
+
+    // Bridges Detection
     public List<List<Integer>> criticalConnections(int n, List<List<Integer>> connections) {
         List<List<Integer>> adj = new ArrayList<>();
 
@@ -53,4 +61,54 @@ public class Tarjan {
             }
         }
     }
+
+
+
+
+    
+    // Articulatuion Points Detection
+    public ArrayList<Integer> articulationPoints(int V, ArrayList<ArrayList<Integer>> adj) {
+        boolean[] visited = new boolean[V];
+        int[] tin = new int[V];
+        int[] low = new int[V];
+        boolean[] mark = new boolean[V];
+        
+        for(int i = 0; i < V; i++) {
+            if(!visited[i]) dfs(i, -1, visited, tin, low, mark, adj);
+        }
+        
+        ArrayList<Integer> ans = new ArrayList<>();
+        for(int i = 0; i < V; i++) {
+            if(mark[i]) ans.add(i);
+        }
+        
+        if(ans.size() == 0) ans.add(-1);
+        return ans;
+    }
+    
+    private int timer = 1; 
+    private void dfs(int node, int parent, boolean[] visited, int[] tin, int[] low, boolean[] mark, ArrayList<ArrayList<Integer>> adj) {
+        visited[node] = true;
+        tin[node] = low[node] = timer;
+        timer++;
+        int child = 0;
+        for(int nb_node : adj.get(node)) {
+            if(nb_node == parent) continue;
+            if(!visited[nb_node]) {
+                dfs(nb_node, node, visited, tin, low, mark, adj);
+                low[node] = Math.min(low[node], low[nb_node]);
+                
+                if(low[nb_node] >= tin[node] && parent != -1) {
+                    mark[node] = true;
+                }
+                child++;
+            } else {
+                low[node] = Math.min(low[node], tin[nb_node]);
+            }
+        }
+        if(child > 1 && parent == -1) {
+            mark[node] = true;
+        }
+    }
 }
+
