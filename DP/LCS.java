664d35f0e89c6d05fab9b99e1c1f4efa85ebb600
Suraj Keshari <surajkeshari260@gmail.com>
2026-01-31 16:16:17 +0530

    LCS-Length && LCS-Printing

--- DP/LCS.java
+++ DP/LCS.java
@@ -66,6 +66,41 @@ public class LCS {
         }
         return prev[n];
     }
+
+
+    // Returns the LCS String
+    public String longestCommonSubsequence4(String text1, String text2) {
+        int m = text1.length(), n = text2.length();
+        int[][] dp = new int[m + 1][n + 1];
+
+        for(int i = 0; i <= m; i++) dp[i][0] = 0;
+        for(int j = 0; j <= n; j++) dp[0][j] = 0;
+
+        for(int i = 1; i <= m; i++) {
+            for(int j = 1; j <= n; j++) {
+                // Matched
+                if(text1.charAt(i - 1) == text2.charAt(j - 1)) dp[i][j] = 1 + dp[i - 1][j - 1];
+
+                    // Not - Matched
+                else dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+            }
+        }
+        int len = dp[m][n];
+        char[] ans = new char[len];
+        int index = len - 1;
+        int i = m, j = n;
+        while(i > 0 && j > 0) {
+            if(text1.charAt(i - 1) == text2.charAt(j - 1)) {
+                ans[index] = text1.charAt(i - 1);
+                index--; i--; j--;
+            }
+            else if(dp[i - 1][j] > dp[i][j - 1]) i--;
+            else j--;
+        }
+        String s = new String(ans);
+        return s;
+    }
 }
+
 
 
